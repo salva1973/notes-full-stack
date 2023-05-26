1d8129f05dc9051756329e3557096fec7131fe32
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2023-05-26 12:59:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notes Full Stack
+
+## Link to the App
+
+[Notes Full Stack](https://notes-full-stack.fly.dev/)
+
+## Set the environment variables on Fly.io
+
+```sh
+fly secrets set MONGODB_URI='...'
+```
---
+++ backend/models/note.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+let database = 'database'
+if (url && url.length > 13) {
+  database = `${url.split('://')[0]}://<username>:<password>${
+    url.split('://')[1].split('@')[1]
+  }`
+}
+
+console.log(`Connecting to ${database}...`)
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Note', noteSchema)
